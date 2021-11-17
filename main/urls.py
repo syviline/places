ddf1642448b41a6ab845f6736267f619811c3c64
Niptlox <Niptlox@users.noreply.github.com>
2021-11-17 16:27:42 +0500

    подтверждение почты

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
+from accounts.views import profile
 from . import views
 from django.urls import path
 
+app_name = "main"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile', profile, name='profile'),
 ]
