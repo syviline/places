873c69bdf35b3f1ab351c8d05ebbba8993b650e5
fuccpuff <fuccpuff@gmail.com>
2021-11-17 10:20:55 +0500

    add model Place & edit requirements

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Place
 
 
 # Create your forms here.
@@ -18,3 +19,20 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class NewPlaceForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ('name', 'visited')
+
+
+# Create a custom date input field, otherwise would get a plain text field
+class DateInput(forms.DateInput):
+    input_type = 'date'  # Override the default input type which is text.
+
+
+class TripReviewForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ('notes', 'date_visited', 'photo')
+        widgets = {'date_visited': DateInput()}
